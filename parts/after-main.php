0873e352bb8bece875d23f38dca45a6995d3d3b1
Phil Cable <pcable@wsu.edu>
2017-11-06 12:11:12 -0800

    Add template parts for outputting custom header and footer
    The footer template contains placeholder output for now.

---
+++ parts/after-main.php
@@ -0,0 +1,34 @@
+<footer id="site-footer" class="gray-background">
+
+	<section class="row halves gutter pad-top social-media">
+		<div class="column one">
+			<p><strong><?php echo esc_html( 'Keep Connected' ); ?></strong></p>
+			<ul>
+				<li><a href="#">wsualumni</a><br>
+				WSU Alumni Association Instagram</li>
+				<li><a href="#">@wsucougfb</a><br>
+				WSU Football Twitter</li>
+				<li><a href="#">@WSUAlumAssoc</a><br>
+				WSU Alumni Association Twitter</li>
+				<li><a href="#">WSUCougars</a><br>
+				WSU Athletics Facebook</li>
+				<li><a href="#">WashingtonStateFootball</a><br>
+				WSU Football Facebook</li>
+			</ul>
+		</div>
+		<div class="column two"></div>
+	</section>
+
+	<section class="row single gutter pad-top footer">
+		<div class="column one">
+			<p>
+				<a href="https://my.wsu.edu/">myWSU</a>
+				<a href="https://access.wsu.edu/">Access</a>
+				<a href="https://policies.wsu.edu/">Policies</a>
+				<a href="https://ucomm.wsu.edu/wsu-copyright-policy/">©</a>
+				<a href="#">Back to top</a>
+			</p>
+		</div>
+	</section>
+
+</footer>
