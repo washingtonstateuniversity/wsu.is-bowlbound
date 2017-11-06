692c3679aaccb04ce85ba35b52254e281caf9b62
Phil Cable <pcable@wsu.edu>
2017-11-06 12:16:35 -0800

    Dequeue Spine scripts and remove Spine output

--- functions.php
+++ functions.php
@@ -11,3 +11,13 @@ add_filter( 'spine_child_theme_version', 'bowl_bound_theme_version' );
 function bowl_bound_theme_version() {
 	return '0.0.1';
 }
+
+add_action( 'wp_enqueue_scripts', 'bowl_bound_enqueue_scripts', 21 );
+/**
+ * Enqueues custom styles.
+ *
+ * @since 0.0.1
+ */
+function bowl_bound_enqueue_scripts() {
+	wp_dequeue_script( 'wsu-spine' );
+}
