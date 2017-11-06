095b8c5eb77226067330a39ff3c29c4fecbc2d4c
Phil Cable <pcable@wsu.edu>
2017-11-06 12:15:43 -0800

    Provide a theme version

--- functions.php
+++ functions.php
@@ -1,1 +1,13 @@
 <?php
+
+add_filter( 'spine_child_theme_version', 'bowl_bound_theme_version' );
+/**
+ * Provides a theme version for use in cache busting.
+ *
+ * @since 0.0.1
+ *
+ * @return string
+ */
+function bowl_bound_theme_version() {
+	return '0.0.1';
+}
