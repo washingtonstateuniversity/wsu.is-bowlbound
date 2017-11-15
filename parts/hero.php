e68e9d33ff26796407e0bfd11493296e1934fa46
Phil Cable <pcable@wsu.edu>
2017-11-15 13:12:00 -0800

    Introduce a template part with the hero section markup

---
+++ parts/hero.php
@@ -0,0 +1,167 @@
+<svg xmlns="http://www.w3.org/2000/svg" class="hero-symbols">
+
+	<symbol id="hero-text--wsu" viewBox="0 0 396 12">
+		<g fill="#282828">
+			<path d="M9.4 11.3L7.5 4l-1.9 7.3H3.2L0.2 0.6h2.5l1.9 7.7 2-7.7h1.8l2 7.7 1.9-7.7h2.6l-3 10.7H9.4z"/>
+			<path d="M26.9 11.3l-0.7-1.8h-4.6L21 11.3h-2.6l4.1-10.7h2.8l4.1 10.7H26.9zM23.9 2.9l-1.7 4.6h3.3L23.9 2.9z"/>
+			<path d="M34 9.8l1.2-1.7c0.8 0.8 1.9 1.4 3.4 1.4 1.2 0 1.9-0.6 1.9-1.2 0-1.9-6.1-0.6-6.1-4.6 0-1.8 1.5-3.2 4-3.2C40 0.5 41.4 1 42.5 2l-1.3 1.7c-0.9-0.8-2-1.2-3.1-1.2 -1 0-1.5 0.4-1.5 1.1 0 1.7 6.1 0.6 6.1 4.5 0 2-1.4 3.4-4.3 3.4C36.4 11.5 34.9 10.8 34 9.8z"/>
+			<path d="M55.9 11.3V6.8h-5v4.5h-2.3V0.6h2.3v4.2h5V0.6h2.3v10.7H55.9z"/>
+			<path d="M64.6 11.3V0.6h2.3v10.7H64.6z"/>
+			<path d="M80.7 11.3l-5.1-7v7h-2.3V0.6h2.3l4.9 6.7V0.6h2.3v10.7H80.7z"/>
+			<path d="M88.8 6c0-3.4 2.6-5.5 5.7-5.5 2.2 0 3.6 1.1 4.4 2.4l-1.9 1c-0.5-0.8-1.4-1.4-2.5-1.4 -1.9 0-3.3 1.5-3.3 3.5s1.4 3.5 3.3 3.5c0.9 0 1.8-0.4 2.3-0.8V7.4h-2.8v-2H99v4.1c-1.1 1.2-2.6 2-4.5 2C91.3 11.5 88.8 9.4 88.8 6z"/>
+			<path d="M107.5 11.3V2.6h-3.1v-2h8.5v2h-3.1v8.7H107.5z"/>
+			<path d="M117.8 6c0-3.2 2.4-5.5 5.6-5.5 3.2 0 5.6 2.3 5.6 5.5 0 3.2-2.4 5.5-5.6 5.5C120.1 11.5 117.8 9.2 117.8 6zM126.6 6c0-2-1.3-3.5-3.2-3.5 -2 0-3.2 1.5-3.2 3.5 0 2 1.3 3.5 3.2 3.5C125.3 9.5 126.6 8 126.6 6z"/>
+			<path d="M142.2 11.3l-5.1-7v7h-2.3V0.6h2.3l4.9 6.7V0.6h2.3v10.7H142.2z"/>
+			<path d="M158.5 9.8l1.2-1.7c0.8 0.8 1.9 1.4 3.4 1.4 1.2 0 1.9-0.6 1.9-1.2 0-1.9-6.1-0.6-6.1-4.6 0-1.8 1.5-3.2 4-3.2 1.7 0 3.1 0.5 4.2 1.5l-1.3 1.7c-0.9-0.8-2-1.2-3.1-1.2 -1 0-1.5 0.4-1.5 1.1 0 1.7 6.1 0.6 6.1 4.5 0 2-1.4 3.4-4.3 3.4C161 11.5 159.5 10.8 158.5 9.8z"/>
+			<path d="M175.4 11.3V2.6h-3.1v-2h8.5v2h-3.1v8.7H175.4z"/>
+			<path d="M192.9 11.3l-0.7-1.8h-4.6l-0.7 1.8h-2.6l4.1-10.7h2.8l4.1 10.7H192.9zM190 2.9l-1.7 4.6h3.3L190 2.9z"/>
+			<path d="M202.3 11.3V2.6h-3.1v-2h8.5v2h-3.1v8.7H202.3z"/>
+			<path d="M213.4 11.3V0.6h7.6v2h-5.3v2.3h5.2v2h-5.2v2.4h5.3v2H213.4z"/>
+			<path d="M235.5 7.1V0.6h2.3V7c0 1.5 0.9 2.5 2.5 2.5 1.6 0 2.5-1 2.5-2.5V0.6h2.3V7c0 2.7-1.6 4.5-4.8 4.5C237.1 11.5 235.5 9.7 235.5 7.1z"/>
+			<path d="M258.9 11.3l-5.1-7v7h-2.3V0.6h2.3l4.9 6.7V0.6h2.3v10.7H258.9z"/>
+			<path d="M267.5 11.3V0.6h2.3v10.7H267.5z"/>
+			<path d="M279.2 11.3l-4.1-10.7h2.6l3 8.2 2.9-8.2h2.6l-4.1 10.7H279.2z"/>
+			<path d="M291.5 11.3V0.6h7.6v2h-5.3v2.3h5.2v2h-5.2v2.4h5.3v2H291.5z"/>
+			<path d="M311.2 11.3l-2.1-3.8h-1.7v3.8h-2.3V0.6h5c2.2 0 3.6 1.5 3.6 3.4 0 1.9-1.2 2.9-2.3 3.2l2.4 4.1H311.2zM311.4 4.1c0-0.9-0.7-1.4-1.6-1.4h-2.4v2.9h2.4C310.7 5.5 311.4 5 311.4 4.1z"/>
+			<path d="M319 9.8l1.2-1.7c0.8 0.8 1.9 1.4 3.4 1.4 1.2 0 1.9-0.6 1.9-1.2 0-1.9-6.1-0.6-6.1-4.6 0-1.8 1.5-3.2 4-3.2 1.7 0 3.1 0.5 4.2 1.5l-1.3 1.7c-0.9-0.8-2-1.2-3.1-1.2 -1 0-1.5 0.4-1.5 1.1 0 1.7 6.1 0.6 6.1 4.5 0 2-1.4 3.4-4.3 3.4C321.5 11.5 320 10.8 319 9.8z"/>
+			<path d="M333.7 11.3V0.6h2.3v10.7H333.7z"/>
+			<path d="M344.8 11.3V2.6h-3.1v-2h8.5v2h-3.1v8.7H344.8z"/>
+			<path d="M359 11.3V7l-4.1-6.3h2.6l2.7 4.3 2.6-4.3h2.6L361.2 7v4.4H359z"/>
+			<path d="M379.1 11.3V0.6h2.3v10.7H379.1z"/>
+			<path d="M387 9.8l1.2-1.7c0.8 0.8 1.9 1.4 3.4 1.4 1.2 0 1.9-0.6 1.9-1.2 0-1.9-6.1-0.6-6.1-4.6 0-1.8 1.5-3.2 4-3.2 1.7 0 3.1 0.5 4.2 1.5l-1.3 1.7c-0.9-0.8-2-1.2-3.1-1.2 -1 0-1.5 0.4-1.5 1.1 0 1.7 6.1 0.6 6.1 4.5 0 2-1.4 3.4-4.3 3.4C389.5 11.5 388 10.8 387 9.8z"/>
+		</g>
+	</symbol>
+
+	<symbol id="hero-text--bowl" viewBox="0 0 445 147">
+		<g fill="#a60f2d">
+			<path d="M84.5 146.6H0.7v-25.4h10.4l0.2-94.5h-11V0.4h84.2l14.6 13v44.8L88.7 70.6l10.4 10.8v49.3L84.5 146.6zM68.9 85.6H39.7v35.6h29.2V85.6zM68.5 27.2H39.9v32h28.6V27.2z"/>
+			<path d="M118.2 146.4l-11.7-11.7V15l13.9-14h59.8l13.7 13.5v118.4l-13.3 13.5H118.2zM165.5 27.1h-30.8v94.1h30.8V27.1z"/>
+			<path d="M258.2 146.4h-25.7L214.8 26.9H206V1h44.5v26.1h-7.2l6.8 68.4 14.6-57.2h18.7l16 56.7 7.7-67.9h-9.4V1h43v26.1h-8.5l-17.1 119.3h-25.7l-15.7-65.5L258.2 146.4z"/>
+			<path d="M390 121h27.2v-19.3h27.5v44.6h-94V121h10.8V26.7h-10.6V1h49.1v26.1H390V121z"/>
+		</g>
+	</symbol>
+
+	<symbol id="hero-text--bound" viewBox="0 0 507 147">
+		<g fill="#4c4c4c">
+			<path d="M84.6 146.4H0.7V121h10.4l0.2-94.5h-11V0.2h84.2l14.6 13V58L88.7 70.4l10.4 10.8v49.3L84.6 146.4zM68.9 85.4H39.8V121h29.2V85.4zM68.6 27.1H39.9v32h28.6V27.1z"/>
+			<path d="M118.2 146.2l-11.7-11.7V14.8l13.9-14h59.8l13.7 13.5v118.4l-13.3 13.5H118.2zM165.6 26.9h-30.8V121h30.8V26.9z"/>
+			<path d="M270.5 146.8h-42.7l-15.5-15.8V26.5h-7.9v-25h39.1v25.4h-6.7v94h24.8v-94h-7V1.5h39.1v25.4h-7.6v104L270.5 146.8z"/>
+			<path d="M348.8 146.4h-47.7v-26.3h10.8V26.3h-11.2V1h42.1l21.1 62.1V25.8h-10.3V1h49.7v25.4h-10.3v93.8h10.3v26.3h-39.2L340 72.2v48.2h8.8V146.4z"/>
+			<path d="M490.3 146.4h-80.1l-0.2-25.2h10.8V26.9H410V0.8h80.6l16 14v117L490.3 146.4zM476.2 26.9h-27v94h27V26.9z"/>
+		</g>
+	</symbol>
+
+</svg>
+
+<section class="hero">
+
+	<svg id="hero" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="1500" height="960" viewBox="0 0 100 100" role="presentation" aria-hidden="true">
+
+		<!-- 1 unit = 9.6 pixels -->
+
+		<defs>
+			<mask id="hero--mask-left">
+				<path fill="#fff" d="M-28.125 0H-16L84 100H-28.125Z"/>
+			</mask>
+			<mask id="hero--mask-right">
+				<path fill="#fff" d="M-16 0H128.125V100H84Z"/>
+			</mask>
+		</defs>
+
+		<image class="left" mask="url(#hero--mask-left)" xlink:href="<?php echo esc_url( get_stylesheet_directory_uri() . '/images/hero-bg-left.jpg' ); ?>" x="-27.5" y="12.5" width="114.58333" height="91.66667"/>
+
+		<g class="right" mask="url(#hero--mask-right)">
+
+			<image xlink:href="<?php echo esc_url( get_stylesheet_directory_uri() . '/images/hero-bg-right.jpg' ); ?>" x="-3.9" y="-4.8" width="120.83333" height="100"/>
+
+			<g class="multiply-blend top">
+
+				<svg xmlns="http://www.w3.org/2000/svg" x="29.5" y="37" width="41.25" height="1.25">
+					<use xlink:href="#hero-text--wsu"/>
+				</svg>
+
+				<svg xmlns="http://www.w3.org/2000/svg" x="29.3" y="40.1" width="46.3541667" height="15.3125">
+					<use xlink:href="#hero-text--bowl"/>
+				</svg>
+
+			</g>
+
+			<rect class="top" fill="#a60f2d" x="29.3" y="55.3" width="61.8" height="1"/>
+
+			<g class="multiply-blend bottom">
+				<svg xmlns="http://www.w3.org/2000/svg" x="47.7" y="58.35" width="52.8125" height="15.3125">
+					<use xlink:href="#hero-text--bound"/>
+				</svg>
+			</g>
+
+			<rect class="bottom" fill="#4c4c4c" x="47.7" y="73.5" width="69.1" height="1"/>
+
+		</g>
+
+		<line class="crimson-line" stroke="#a60f2d" stroke-width=".2" x1="-6" y1="10" x2="84" y2="100"/>
+
+		<line class="white-line" stroke="#fff" stroke-width=".1" x1="-14.5" y1="10" x2="75.5" y2="100"/>
+
+		<image class="celebrators" xlink:href="<?php echo esc_url( get_stylesheet_directory_uri() . '/images/hero-celebrators.png' ); ?>" x="-4.45" y="13.25" width="33.645" height="63.854"/>
+
+	</svg>
+
+	<svg id="hero-mobile" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="1500" height="960" viewBox="0 0 100 100" role="presentation" aria-hidden="true">
+
+		<defs>
+			<mask id="hero--mask-mobile">
+				<path fill="#fff" d="M0 -13.3l100 100V125H0Z"/>
+			</mask>
+		</defs>
+
+		<image xlink:href="<?php echo esc_url( get_stylesheet_directory_uri() . '/images/hero-bg-right.jpg' ); ?>" x="-48" y="-19.5" width="192.125" height="159" class="hero--mobile-background"/>
+
+		<g class="multiply-blend top">
+
+			<svg xmlns="http://www.w3.org/2000/svg" x="17.5" y="-4" width="41.25" height="1.25">
+				<use xlink:href="#hero-text--wsu"/>
+			</svg>
+
+			<svg xmlns="http://www.w3.org/2000/svg" x="17.3" y="-1.1" width="46.3541667" height="15.3125">
+				<use xlink:href="#hero-text--bowl"/>
+			</svg>
+
+		</g>
+
+		<rect class="top" fill="#a60f2d" x="17.3" y="14" width="61.8" height="1"/>
+
+		<g class="multiply-blend bottom">
+
+			<svg xmlns="http://www.w3.org/2000/svg" x="35.7" y="17.35" width="52.8125" height="15.3125">
+				<use xlink:href="#hero-text--bound"/>
+			</svg>
+
+		</g>
+
+		<rect class="bottom" fill="#4c4c4c" x="35.7" y="32.5" width="69.1" height="1"/>
+
+		<image xlink:href="<?php echo esc_url( get_stylesheet_directory_uri() . '/images/hero-bg-right.jpg' ); ?>" x="-48" y="-19.5" width="192.125" height="159" mask="url(#hero--mask-mobile)" class="hero--mobile-background"/>
+
+		<line class="crimson-line" stroke="#a60f2d" stroke-width=".2" x1="0" y1="-13.3" x2="75" y2="61.7"/>
+
+		<line class="white-line" stroke="#fff" stroke-width=".1" x1="0" y1="-4.3" x2="66" y2="61.7"/>
+
+		<image class="celebrators" xlink:href="<?php echo esc_url( get_stylesheet_directory_uri() . '/images/hero-celebrators.png' ); ?>" x="4" y="6.5" width="43" height="81.6"/>
+
+	</svg>
+
+	<svg id="hero--spirit-mark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 168.5 165.5">
+
+		<title>Washington State University spirit mark</title>
+
+		<path d="m13.1,158.1c0,0 0.1,0 0,0c20.8,-4.7 36.6,-29 41.3,-38.7l3,-6.2l1.8,6.6c0.1,0.4 2,7.6 2,17.3c0,1.1 0,2.2 -0.1,3.3c7.1,0.3 12.2,-1.2 15.4,-4.5c4.5,-4.7 5.9,-13.7 4.2,-27.4c-0.8,-6.6 -3.2,-17 -5.4,-26.2c-1.1,-4.7 -2.1,-9.1 -2.7,-12.3c-4,-20.1 1,-32.6 5.8,-39.5c0.8,-1.1 1.6,-2.1 2.5,-3.1c-14.3,4.8 -26.3,12.4 -38.8,24.8l8.7,10.2l3.6,-3.3c3.4,12.6 1,24.2 1,24.2c-4.6,-7 -10,-12.7 -10,-12.7l2.4,-4l-7.9,-9.2l-0.9,-1c-9.7,13.3 -12.8,24.2 -12.8,24.2l6.9,6.3l4,3.6l1.3,-3.9c7.4,13.8 0.7,26.8 0.7,26.8c-1.2,-4.7 -6.1,-11.7 -6.1,-11.7l1.9,-6.3l-10.2,-9.3c-10.6,33.3 -24.8,44.1 -24.8,44.1s15.4,-2.8 23.9,-5.7c0,0.1 -0.3,17.7 -10.7,33.6z" id="hero--spirit-mark-w" fill="#a60f2d"/>
+
+		<path d="m168.1,38.1l-24.5,1c0,2.9 -0.4,5.8 -1.3,8.6c2.3,10 0.7,15.5 0.7,15.5c-4.1,-9.6 -7.3,-12.2 -7.3,-12.2c-22.5,-10.1 -35.2,12.6 -33.8,27.3c1.1,12 5.5,28 8.9,53c2.9,21.1 -7.8,30.2 -20,32.3c-0.3,0 -0.5,0.1 -0.8,0.1c-0.1,0 -0.1,0 -0.2,0c-2,0.3 -4.1,0.3 -6.3,0.3c-0.1,0 -0.2,0 -0.2,0c-0.2,0 -0.5,0 -0.7,0c-1,-0.1 -1.9,-0.2 -2.8,-0.3c-9.5,-1.3 -30.8,-5.2 -52.9,-5.7c13,-7.6 23.1,-20.7 28.7,-30c0.3,2.6 0.6,5.7 0.6,9.2c0,1.7 -0.1,3.5 -0.2,5.4l-0.2,2.5l2.5,0.2c10.1,0.9 17.3,-1.1 22,-5.9c5.8,-6 7.5,-16 5.6,-31.5c-0.9,-6.8 -3.3,-17.4 -5.5,-26.7c-1.1,-4.7 -2.1,-9.1 -2.7,-12.1c-3.6,-18.3 0.7,-29.4 5,-35.5c3.4,-4.8 8,-8.3 13.1,-10c0.1,0 0.2,0 0.3,0l8.4,-23.7l2.6,0l-5.6,22.7c1.3,-0.2 2.6,-0.4 4,-0.6l7.2,-20.8l2.6,0l-4.7,20.1c7.5,-1 15.6,-1.7 24.6,-2.4c1.5,0.6 3,2.3 4.4,4.6l23.6,-7.3l0.9,2.5l-22.5,8.8c0.2,0.5 0.3,1 0.5,1.4l24.7,-3.1l0.4,2.7l-24.2,4.8c0.1,0.5 0.2,0.9 0.2,1.3l24.9,0.8l0,2.7zm-71,11.1c4.9,-3.5 13.4,-6.2 21.2,-5.5c2,0.2 3.7,-0.4 1.5,-1.5c-5.9,-3.1 -19.7,-2.1 -24.5,1.3c-7.8,5.3 -6.5,14.8 -6.3,15.1c1.6,-2.9 4.2,-6.7 8.1,-9.4z" id="hero--spirit-mark-s" fill="#a60f2d"/>
+
+		<path d="m148.2,84.2c0,0 5.2,28.3 -13.2,29.3c-17.7,0.9 -26.1,-25.8 -26.1,-25.8s4.3,52.3 29.1,48.8c28,-3.8 10.2,-52.3 10.2,-52.3z" id="hero--spirit-mark-u" fill="#a60f2d"/>
+
+		<path d="m102.4,164.9c0,0 8.5,-3.6 13.2,-17.7c4.1,8.6 3,17.5 3,17.5c-3.7,0.7 -16.2,0.2 -16.2,0.2z" id="hero--spirit-mark-dot" fill="#a60f2d"/>
+
+	</svg>
+
+</section>
