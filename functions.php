ae54270c8c1e0c7edac525cfc681397ff1722c28
Phil Cable <pcable@wsu.edu>
2017-11-21 09:30:10 -0800

    Bump version 0.0.3

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ add_filter( 'spine_child_theme_version', 'bowl_bound_theme_version' );
  * @return string
  */
 function bowl_bound_theme_version() {
-	return '0.0.2';
+	return '0.0.3';
 }
 
 add_action( 'wp_enqueue_scripts', 'bowl_bound_enqueue_scripts', 21 );
